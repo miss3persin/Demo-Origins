f2c1eb64d8304de78bac75da0c3b9466af7dbc09
miss3persin <ige-bello.oluwasegunfunmi@lmu.edu.ng>
2024-09-24 13:13:13 +0100

    changed instagram link

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -8,7 +8,7 @@ export const Footer = () => {
         <p className='text-gray-900 text-sm lg:text-base dark:text-[#ACA992]'>Novel and Website Created by miss3persin</p>
         <div className='flex gap-8 items-center justify-center'>
             <div><Link href='https://t.me/miss3persin' target='_blank'><Image src='/telegram.png' alt='telegram' width={30} height={30}/></Link></div>
-            <div><Link href='https://www.instagram.com/this_is_segun/' target='_blank'><Image src='/instagram.png' alt='instagram' width={30} height={30}/></Link></div>
+            <div><Link href='https://www.instagram.com/demo_origins/' target='_blank'><Image src='/instagram.png' alt='instagram' width={30} height={30}/></Link></div>
             <div><Link href='https://x.com/miss3persin' target='_blank'><Image src='/X.png' alt='X' width={30} height={30}/></Link></div>
         </div>
     </div>
