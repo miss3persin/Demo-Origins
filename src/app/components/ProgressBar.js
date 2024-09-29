fb861189b63c73552fb1846cdd210096fbd664b0
miss3persin <ige-bello.oluwasegunfunmi@lmu.edu.ng>
2024-09-29 10:07:07 +0100

    minor progress bar fixes

--- src/app/components/ProgressBar.js
+++ src/app/components/ProgressBar.js
@@ -41,7 +41,7 @@ export default function ProgressBar({ chapterId }) {
     >
       <div className="w-full max-w-52 sm:max-w-64 md:max-w-72 h-1 lg:h-2 lg:max-w-96 xl:max-w-[35rem] bg-black/30 dark:bg-black dark:border border-[#ACA992] rounded-full">
         <div
-          className="h-[0.1rem] lg:h-[0.4rem] bg-blue-500 dark:bg-[#ACA992] rounded-full"
+          className="h-1 dark:h-[0.1rem] lg:h-2 dark:lg:h-[0.4rem] bg-blue-500 dark:bg-[#ACA992] rounded-full"
           style={{ width: `${progress}%` }}
         />
       </div>
